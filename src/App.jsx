91d5551d0960a4defce98c390ef758e5ee47c2a2
divyagramaswamy <dramaswamy769@gmail.com>
2026-09-07 20:45:42 -0400

    oops forgot to commit these earlier

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
+import {Routes, Route, NavLink} from 'react-router-dom'
+import Home from './pages/Home'
+import Experience from './pages/Experience'
+import Projects from './pages/Projects'
 
 function App() {
   return (
-    <div className="hero">
-    <h1>Divya Ramaswamy</h1>
-    <p className = "subtitle">Computer Science @ Georgia Tech </p>
-    <p> I'm currently a first year master's student at Georgia Tech. I love and am interested in many sectors of computer science, from computer vision to ... blah blah blah</p>
-  <a href="https://github.com/divyagramaswamy?tab=repositories" target="_blank" rel="noopener noreferrer">
-    Github
-  </a>
-    </div>
+    <>
+      <nav>
+        <NavLink to="/" end > Home</NavLink>
+        <NavLink to="/experience">Work Experience</NavLink>
+        <NavLink to="/projects">Projects</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      <Route path="/experience" element={<Experience />} />
+      <Route path="/projects" element={<Projects />} />
+    </Routes>
+    </>
 
   )
 }
